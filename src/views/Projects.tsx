fcd073650b9b1254e6a16b99115565f328745a51
fwsegzzie <kola.victor14@gmail.com>
2025-09-03 13:41:40 +0100

    okay

--- src/views/Projects.tsx
+++ src/views/Projects.tsx
@@ -115,19 +115,22 @@ const Projects = () => {
                     View Code
                   </a>
                   
-                  {project.liveDemo && (
-                    <a 
-                      href={project.liveDemo} 
-                      target="_blank" 
-                      rel="noopener noreferrer"
-                      className="flex items-center bg-green-600 hover:bg-green-700 text-white font-medium py-2 px-4 rounded"
-                    >
-                      <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
-                      </svg>
-                      Live Demo
-                    </a>
-                  )}
+               {project.liveDemo && (
+  <a 
+    href={project.liveDemo} 
+    target="_blank" 
+    rel="noopener noreferrer"
+    className="flex items-center bg-green-600 hover:bg-green-700 text-white font-medium 
+               py-1 px-2 text-xs sm:py-2 sm:px-4 sm:text-base rounded"
+  >
+    <svg className="w-3.5 h-3.5 mr-1 sm:w-5 sm:h-5 sm:mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+    </svg>
+    Live Demo
+  </a>
+)}
+
+
                   
                   <button 
                     onClick={() => openModal(project)}
